620586b54458210350a652fc1beae597649a5c3e
Pedro Henrique Pereira Sales <sales_pedro@terceiros.sicredi.com.br>
2022-06-21 19:00:57 -0300

    fix: code writing

--- src/presentation/routes/message.ts
+++ src/presentation/routes/message.ts
@@ -4,29 +4,27 @@ import { router } from '../../config/express.config'
 export const AssistantMessageMakeRouter = router
 
 AssistantMessageMakeRouter.post('/send', (request, response) => {
-    try {
-        const { message } = request.body
-        if (message == null) {
-            return response.status(400).json({
-                error: 'Missing field \'message\''
-            })
-        }
-
-        new AssistantTextMessage().createSession()
-            .then(newSessionId => {
-                new AssistantTextMessage().send(message, newSessionId)
-                    .then(AssistantResponse => {
-                        return response.status(AssistantResponse.status).json({
-                            intents: AssistantResponse.result.output.intents,
-                            entities: AssistantResponse.result.output.intents,
-                            response: AssistantResponse.result.output.generic
-                        })
-                    })
-            })
-
-    } catch (error) {
-        return response.status(500).json({
-            error: error
-        })
+  try {
+    const { message } = request.body
+    if (message == null) {
+      return response.status(400).json({
+        error: 'Missing field \'message\''
+      })
     }
+
+    void new AssistantTextMessage().createSession().then(newSessionId => {
+      void new AssistantTextMessage().send(message, newSessionId)
+        .then(AssistantResponse => {
+          return response.status(AssistantResponse.status).json({
+            intents: AssistantResponse.result.output.intents,
+            entities: AssistantResponse.result.output.intents,
+            response: AssistantResponse.result.output.generic
+          })
+        })
+    })
+  } catch (error) {
+    return response.status(500).json({
+      error: error
+    })
+  }
 })
